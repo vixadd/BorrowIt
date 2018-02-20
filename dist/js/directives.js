8a48b708e7554d8b0ca700f4f5f4f3896ee36fa2
vixadd <dkroell@acm.org>
2018-02-19 23:14:51 -0500

    Initial Commit for the cleanup

--- dist/js/directives.js
+++
@@ -1,5 +0,0 @@
-define(['directives/header', 'directives/footer'], function(headerDirective, footerDirective) {
-	'use strict';
-	window.app.directive('ngheader', headerDirective);
-	window.app.directive('ngfooter', footerDirective);
-});
