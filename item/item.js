c88a99366c8f478c67ddf795f5a4685ea0884bfa
Jonah <Jonah.lazar.14@cnu.edu>
2018-04-03 01:14:00 -0400

    Finish hardcoded item view

--- item/item.js
+++ item/item.js
@@ -26,8 +26,19 @@ app.controller('ItemController', ['$routeParams', function($routeParams) {
 				owner: "OWNER-UID-HERE",
 				pic: "http://www.gaebler.com/images/startbiz/Used-Bicycle-Shop.jpg",
 				maxBorrowTime: 60,
-				description: "Red, slightly used, but it will get you where you're going.",
-				comments: ["Great bike. Would ride again.", "A little shaky, but it didn't fall apart on me."],
+				description: "Red, slightly used, but it will get you where you're going. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+				comments: [
+					{
+						text: "Great bike. Would ride again. Any questions?",
+						userName: "DavidSPumpkins",
+						userId: "ANOTHER-UID"
+					},
+					{
+						text:  "A little shaky, but it didn't fall apart on me.",
+						userName: "MichaelJFox",
+						userId: "YET-ANOTHER-UID"
+					}
+				],
 				condition: 3,
 				available : true
 			}
@@ -35,7 +46,7 @@ app.controller('ItemController', ['$routeParams', function($routeParams) {
 		
 		vm.getItemOwner = function(item){
 			vm.itemOwner = {
-				firstName: "John",
+				userName: "HarlEDavidson",
 				reputation: 245,
 				userId: "OWNER-UID-HERE",
 				profilePic: "url-to-profile-pic"
