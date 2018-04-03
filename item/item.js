31318d5aaf019bdb903a0cdee53115d87e535a5b
Jonah <Jonah.lazar.14@cnu.edu>
2018-04-02 23:15:10 -0400

    Start editing single item view

--- item/item.js
+++ item/item.js
@@ -4,7 +4,8 @@ app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 		.when('/item/:id', {
 			templateUrl: '/item/item.html',
-			controller: 'ItemController'
+			controller: 'ItemController',
+			controllerAs: 'vm'
 		 })
 }])
 
@@ -21,12 +22,13 @@ app.controller('ItemController', ['$routeParams', function($routeParams) {
 		
 		vm.getItem = function(itemId){
 			vm.item = {
-				name : "Bicycle",
+				name : "Red Bicycle",
 				owner: "OWNER-UID-HERE",
-				pics: [],
+				pics: ["http://www.gaebler.com/images/startbiz/Used-Bicycle-Shop.jpg", "https://www.roysbike.com/images/schwinn_used_big.jpg", "https://pedalrevolutionblog.files.wordpress.com/2011/11/0091.jpg"],
 				maxBorrowTime: 60,
-				description: "",
-				condition: 6	
+				description: "Red, slightly used, but it will get you where you're going.",
+				condition: 6,
+				available : true
 			}
 		}
 		
