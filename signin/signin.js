fea938a5e5ef5f64c7865b4057cfec8252310b40
Jonah <Jonah.lazar.14@cnu.edu>
2018-04-09 23:20:39 -0400

    Add signin/signout feature

--- signin/signin.js
+++ signin/signin.js
@@ -5,15 +5,53 @@ angular.module('BorrowIt')
 	    controller: 'SigninController',
 	    controllerAs: 'vm'
 	})
-    }]).controller('SigninController', ['$log', function($log) {
-	var vm = this;
-
-	vm.onSigninClick = function() {
-
-	    // Debug values
-	    $log.log(vm.inputEmail);
-	    $log.log(vm.inputPassword);
-	    $log.log(vm.remember_me);
-	}
+    }]).controller('SigninController', ['$scope', '$location', function($scope, $location) {
+		var vm = this;
+		
+		vm.username;
+		vm.password;
+		
+		vm.submitEnabled = true;
+		
+		vm.showSignInError = false;
+		vm.errorText = "";
+	
+		vm.submit = function() {
+			if(vm.submitEnabled) {
+				vm.submitEnabled = false;
+				var authenticationData = {
+			        Username : vm.username,
+			        Password : vm.password,
+			    };
+			    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+			    var poolData = { 
+			    	UserPoolId : 'us-east-1_2tWi4ARGA', // Your user pool id here
+				    ClientId : '1a9tue69nuhci0j7e4t0i83foc' // Your client id here
+			    };
+			    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+			    var userData = {
+			        Username : vm.username,
+			        Pool : userPool
+			    };
+			    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+			    cognitoUser.authenticateUser(authenticationDetails, {
+			    	
+			        onSuccess: function (result) {
+			            console.log('access token + ' + result.getAccessToken().getJwtToken());
+			            /*Use the idToken for Logins Map when Federating User Pools with identity pools or when passing through an Authorization Header to an API Gateway Authorizer*/
+			            console.log('idToken + ' + result.idToken.jwtToken);
+			        },
+	
+			        onFailure: function(err) {
+			        	$scope.$apply(function() {
+			        		vm.errorText = err.message;
+				            vm.showSignInError = true;
+				            vm.submitEnabled = true;
+			        	});
+			        },
+	
+			    });
+			}
+		}
 
     }]);
